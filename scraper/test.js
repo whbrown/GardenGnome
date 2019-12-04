7c26f43ead92dceaa34fb81deb2880e60f8c0c2f
William Brown <hce@WmHB.local>
2019-12-04 23:37:26 +0100

    Fixing bugs with scraper

--- scraper/test.js
+++ scraper/test.js
@@ -1,6 +1,11 @@
-const one = 'Habit\nBushy';
-const two = 'Hardiness\n\nH1C';
+console.log('blah'.split());
+// let [characteristicTitle, ...description] = one.split('\n');
+// // description = description.filter(value => value);
+// console.log(characteristicTitle, description.join(''));
 
-let [characteristicTitle, ...description] = one.split('\n');
-// description = description.filter(value => value);
-console.log(characteristicTitle, description.join(''));
+// ? can this outrageously long property access be shortened?
+// ? doesn't seem to work if assigned to a variable
+
+// concat to existing colour array if exists, else just assign
+
+// * sorry for the desperately ugly syntax in the rest of this, in need of refactoring
