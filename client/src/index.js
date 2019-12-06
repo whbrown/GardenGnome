5858e184c6c50c59c10669a2559d4ee8d14c75ba
William Brown <hce@WmHB.local>
2019-12-06 11:33:38 +0100

    testing front end

--- client/src/index.js
+++ client/src/index.js
@@ -7,17 +7,18 @@ import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 
-axios.get("/api/auth/loggedin").then(response => {
-  const user = response.data;
+// axios.get("/api/auth/loggedin").then(response => {
+//   console.log(response);
+//   const user = response.data;
 
   ReactDOM.render(
     <BrowserRouter>
-      {/* <App /> */}
-      <App user={user} />
+      <App />
+      {/* <App user={user} /> */}
     </BrowserRouter>,
     document.getElementById("root")
   );
-});
+// });
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
