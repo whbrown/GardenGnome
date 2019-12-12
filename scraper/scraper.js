f280828a188116f77f0aa03147397fe3ca8b875d
William Brown <hce@WmHB.local>
2019-12-12 15:32:54 +0100

    set scaper RHSID start at last created rather than highest rhsid to facilitate  out of order scraping

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -8,8 +8,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/gardengnome');
 (async () => {
   // add { headless: false } as launch param
   const browser = await puppeteer.launch();
-  const mostRecentPlant = await Plant.findOne({}).sort({ RHSID: -1 });
-  for (let RHSID = mostRecentPlant.RHSID + 1; RHSID < 1000000; RHSID++) {
+  const mostRecentPlant = await Plant.findOne({}).sort({ created_at: -1 });
+  for (let { RHSID } = mostRecentPlant; RHSID < 1000000; RHSID++) {
     const page = await browser.newPage();
     page.setDefaultNavigationTimeout(99999999);
     await page.goto(
