3022056e95a2752ebf637b0a427ec2694612eb26
William Brown <hce@WmHB.local>
2019-12-13 06:07:07 +0100

    build version

--- client/build/precache-manifest.f8e734738e9b0f218b48ad39ebe32313.js
+++ client/build/precache-manifest.f8e734738e9b0f218b48ad39ebe32313.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8ae8c45452e79a9605473a52823a1b1f",
+    "revision": "5cc3afa00107a79467fd12137b0fa2f9",
     "url": "/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.64a99b10.chunk.css"
   },
   {
-    "revision": "885ff28931ed0272b80c",
-    "url": "/static/css/main.41a0535f.chunk.css"
+    "revision": "f277f26dcd526e974e8a",
+    "url": "/static/css/main.09da6c70.chunk.css"
   },
   {
     "revision": "9520c94fbbb7c08834e0",
     "url": "/static/js/2.533ffa28.chunk.js"
   },
   {
-    "revision": "885ff28931ed0272b80c",
+    "revision": "f277f26dcd526e974e8a",
     "url": "/static/js/main.95012fa1.chunk.js"
   },
   {
