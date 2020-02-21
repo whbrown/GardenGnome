9ad72cf6d684de3b2b23ad9848cf451c1220f465
William Brown <hce@WmHB.local>
2020-02-21 00:29:04 -0500

    add build

---
+++ client/build/precache-manifest.4e146f71ea3bd4567719a97cf7174779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296822b496fc671d8c1cbc9176a08b4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9520c94fbbb7c08834e0",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "cdc263a439a192426c34",
+    "url": "/static/css/main.9ee4fa97.chunk.css"
+  },
+  {
+    "revision": "9520c94fbbb7c08834e0",
+    "url": "/static/js/2.533ffa28.chunk.js"
+  },
+  {
+    "revision": "cdc263a439a192426c34",
+    "url": "/static/js/main.1fd3b700.chunk.js"
+  },
+  {
+    "revision": "6718c81cb616e4f2cf7d",
+    "url": "/static/js/runtime-main.bf27dfde.js"
+  }
+]);
