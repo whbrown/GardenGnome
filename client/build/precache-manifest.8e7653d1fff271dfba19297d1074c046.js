64ae11419326aa5abef3ca5b23c33c73476c4269
William Brown <hce@WmHB.local>
2020-02-20 23:33:39 -0500

    fix build recognition issue

---
+++ client/build/precache-manifest.8e7653d1fff271dfba19297d1074c046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c250b3af01ba03032b5de5036c72214f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9520c94fbbb7c08834e0",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "18f8374a1c81a08b206c",
+    "url": "/static/css/main.9ee4fa97.chunk.css"
+  },
+  {
+    "revision": "9520c94fbbb7c08834e0",
+    "url": "/static/js/2.533ffa28.chunk.js"
+  },
+  {
+    "revision": "18f8374a1c81a08b206c",
+    "url": "/static/js/main.e74cba7c.chunk.js"
+  },
+  {
+    "revision": "6718c81cb616e4f2cf7d",
+    "url": "/static/js/runtime-main.bf27dfde.js"
+  }
+]);
