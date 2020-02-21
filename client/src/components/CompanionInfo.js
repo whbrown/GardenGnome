ef4809f37b0d619d1827021446f021dec9c4ba6a
William Brown <hce@WmHB.local>
2020-02-20 19:06:28 -0500

    minor fix homepage and companion styling

--- client/src/components/CompanionInfo.js
+++ client/src/components/CompanionInfo.js
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 export default class CompanionInfo extends Component {
   render() {
     return (
-      <table className="table-container">
+      <table className="table-container" >
           <thead>
             <tr>
               <th>Common name</th>
@@ -23,16 +23,16 @@ export default class CompanionInfo extends Component {
               this.props.companionPlants.map((plant, index) => {
                 return (
                   <tr>
-                    <td style={{marginTop: '50vh'}}><h1 className="tableh1">{plant.commonName}</h1></td>
-                    <td><button className="learn-more" style={{alignSelf: 'center', justifySelf: 'center', margin: '1em 7em 2em'}} onClick={this.props.resetCompanionsTable}>Return to Garden</button></td>
-                    <td><h1 className="tableh1">Attracts: </h1><ul>{plant.attracts.map((attraction, index) => <li key={index}><h2 className='tableh2'>{attraction}</h2></li>)}</ul></td>
+                    <td style={{marginTop: '5vh'}}><h1 className="tableh1">{plant.commonName}</h1></td>
+                    <td style={{display: 'flex', justifyContent: 'center'}}><button className="learn-more" style={{margin: '1em 0'}} onClick={this.props.resetCompanionsTable}>Return to Garden</button></td>
+                    <td><h1 className="tableh1">Attracts: </h1><ul>{plant.attracts.map((attraction, index) => <li key={index}><h2 className='tableh2'>{attraction || 'None'}</h2></li>)}</ul></td>
                     
-                    <td><h1 className="tableh1">Helper: </h1><ul>{plant.helpedBy.map((helper, index) => <li key={index}><h2 className='tableh2'>{helper}</h2></li>)}</ul></td>
-                    <td><h1 className="tableh1">Helped: </h1><ul>{plant.helps.map((helped, index) => <li key={index}><h2 className='tableh2'>{helped}</h2>)</li>)}</ul></td>
-                    <td><h1 className="tableh1">Repelled: </h1><ul>{plant.repels.map((repelled, index) => <li key={index}><h2 className='tableh2'>{repelled}</h2>)</li>)}</ul></td>
-                    <td><h1 className="tableh1">To Avoid: </h1><ul>{plant.avoid.map((toAvoid, index) => <li key={index}><h2 className='tableh2'>{toAvoid}</h2>)</li>)}</ul></td>
-                    <td><h1 className="tableh1">Comments: </h1><h2 className='tableh2'>{plant.comments}</h2></td>
-                    <td><h1 className="tableh1">Category: </h1><h2 className='tableh2'>{plant.category}</h2></td>
+                    <td><h1 className="tableh1">Helper: </h1><ul>{plant.helpedBy.map((helper, index) => <li key={index}><h2 className='tableh2'>{helper || 'None'}</h2></li>)}</ul></td>
+                    <td><h1 className="tableh1">Helped: </h1><ul>{plant.helps.map((helped, index) => <li key={index}><h2 className='tableh2'>{helped || 'None'}</h2></li>)}</ul></td>
+                    <td><h1 className="tableh1">Repelled: </h1><ul>{plant.repels.map((repelled, index) => <li key={index}><h2 className='tableh2'>{repelled || 'None'}</h2></li>)}</ul></td>
+                    <td><h1 className="tableh1">To Avoid: </h1><ul>{plant.avoid.map((toAvoid, index) => <li key={index}><h2 className='tableh2'>{toAvoid || 'None'}</h2></li>)}</ul></td>
+                    <td><h1 className="tableh1">Comments: </h1><h2 className='tableh2'>{plant.comments || 'None'}</h2></td>
+                    <td><h1 className="tableh1">Category: </h1><h2 className='tableh2'>{plant.category || 'None'}</h2></td>
                     <td><button className="learn-more" style={{alignSelf: 'center', justifySelf: 'center', margin: '1em 7em 2em'}} onClick={this.props.resetCompanionsTable}>Return to Garden</button></td>
                   </tr>
                   
--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -8,10 +8,20 @@ const Home = styled.div`
   /* background-image: url('https://images.unsplash.com/photo-1515150144380-bca9f1650ed9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'); */
   background-image: url('https://images.unsplash.com/photo-1534710961216-75c88202f43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80');
   background-size: cover;
-  height: 100vh;
+  /* height: 120vh; */
   /* background-position: center; */
   background-color: hsla(0,0%,100%,0.7);
   background-blend-mode: overlay;
+  height: 90vh;
+  overflow: hidden;
+  img {
+    width: 300px;
+  }
+  @media (min-width: 1000px) {
+    img {
+      width: 400px;
+    }
+  }
 `
 
 class Homepage extends Component {
@@ -24,8 +34,8 @@ class Homepage extends Component {
   render() {
     // console.log(this.props.user)
     return (
-      <Home className="fadeIn Homepage" style={{ textAlign: "center", marginBottom: "80px", padding: "1rem" }} >
-        <img src="../../assets/gnome.svg" alt="Garden Gnome" style={{ width: "40vw", marginTop: "80px" }} />
+      <Home className="fadeIn Homepage" style={{ textAlign: "center" }} >
+        <img src="../../assets/gnome.svg" alt="Garden Gnome" style={{ marginTop: "80px" }} />
         <h1 style={{ color: "Green", fontWeight: "bolder" }}>Gnome</h1>
         <p style={{ color: "#333", fontWeight: "400" }}>Your pocket guide to growing healthy and happy plants. </p>
         <p style={{ color: "#333", fontWeight: "400" }}>Access our database of over 300,000 plants and their in-depth information on care and maintenance</p>
